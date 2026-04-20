9285e075b74f03176c5bbb793180ef89a0127903
ooiyeefei <yeefeiooi@gmail.com>
2026-04-20 05:16:15 +0000

    Add skills library and strengthen rebase skill tests

---
+++ atelier/skills/__init__.py
@@ -0,0 +1,11 @@
+from atelier.skills.loader import DEFAULT_SKILLS_DIR, SkillLoader, SkillNotFoundError, get_skill
+from atelier.skills.schema import Skill, SkillFrontmatter
+
+__all__ = [
+    "DEFAULT_SKILLS_DIR",
+    "Skill",
+    "SkillFrontmatter",
+    "SkillLoader",
+    "SkillNotFoundError",
+    "get_skill",
+]
